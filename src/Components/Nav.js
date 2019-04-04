06b149a6b55b28c6126f2e28966ac945dd0a8bbb
Kevin Ung <ung.kevin78@gmail.com>
2019-04-04 17:06:08 -0600

    restyled nav comp with styled component

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,24 +3,51 @@ import List from './List';
 import Friends from './Friends';
 import Group from './Group';
 import Individual from './Individual';
+import styled from 'styled-components'
 
 const icons = {fontSize:'65px',color:'lightgrey'}
+const NavBody = styled.div`
+    margin-left:1vw;
+    width:10vw;
+    background:#303841;
+    height:97vh;
+    display:flex;
+    flex-direction:column;
+    margin-top:1.5vh;
+    border-radius:10px 0px 0px 10px;
+    margin-bottom:1.5vh
+`
+const NavTop = styled.div`
+    height:25%;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    justify-content:space-between;
+`
+
+const ActiveDot = styled.div`
+    display:flex;
+    margin-top:10px;
+    margin-left:-55px;
+`
+
+
+
 class Nav extends Component {
     render(){
         console.log(this.props.updateState)
         const {updateState} = this.props;
         return(
-            <div style={{marginLeft:'1vw',width:'10vw',background:'#303841',height:'97vh',display:'flex',flexDirection:'column',marginTop:'1.5vh',borderRadius:'10px 0px 0px 10px',marginBottom:'1.5vh'}}>
-                    {/* <img src='' alt='Profile Pic'/> */}
-                <div style={{height:'25%',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-between'}}>
-                    <div id="active-dot" style={{display:'flex',marginTop:10,marginLeft:'-55px'}}>
+            <NavBody>
+                <NavTop>
+                    <ActiveDot id="active-dot">
                         <div className="active-dot" id="red-dot"></div>
                         <div className="active-dot" id="yellow-dot"></div>
                         <div className="active-dot" id="green-dot"></div>
-                    </div>
+                    </ActiveDot>
                     <img src='https://s3-us-west-1.amazonaws.com/marketin/Hermoine.jpeg' alt='profile' style={{height:'60%',width:'75%',borderRadius:'50%',background:'gold',position:'relative',filter:'grayscale(100%)'}}/>
                     <div style={{height:15,width:15,background:'green',borderRadius:'50%',boxShadow:'-1px -1px 3px 2px black',position:'absolute',top:'150px',left:'120px'}}></div>
-                </div>
+                </NavTop>
 
                 <div style={{display:'flex',flexDirection:'column',justifyContent:'space-evenly',height:'60%',alignItems:'center'}}>
                     <i onClick={()=>updateState(List)} className="fas fa-comment" style={icons} id="nav-chat-icon"></i>
@@ -31,7 +58,7 @@ class Nav extends Component {
                 <div style={{display:'flex',alignItems:'center',height:'15%',justifyContent:'center'}}>
                     <i className="fas fa-power-off" style={icons}></i>
                 </div>
-            </div>
+            </NavBody>
         )
     }
 
