4ee73cb6958a2befccccb9009d3304d31760bc3f
Kevin Ung <ung.kevin78@gmail.com>
2019-04-30 16:50:48 -0600

    individual view chat

--- src/Components/Individual.js
+++ src/Components/Individual.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {selectedFriend,updateRoom} from '../ducks/reducer';
+import socket from './Sockets'
 
 class Individuals extends Component {
     constructor(){
@@ -8,13 +11,60 @@ class Individuals extends Component {
         }
     }
     
+    startChat(userId,user){
+        this.props.selectedFriend(user)
+        const {id} = user;
+        socket.emit('endChat',this.props.room);
+        let bigger;
+        let smaller;
+        if(userId > id){
+            bigger = userId;
+            smaller = id
+        } else {
+            bigger = id;
+            smaller = userId;
+        } 
+        let room = bigger+':'+smaller;
+        this.props.updateRoom(room)
+        socket.emit('startChat',{room});
+    }
+
     render(){
+        const userId = this.props.user.id;
+        const {search,chats} = this.props
+        const mappedChats = chats.filter(user=>{
+            const friendSearch = search.toLowerCase().split(' ')
+              for (let i = 0; i < friendSearch.length; i++) {
+                const searchName = friendSearch[i];
+                if (!user.first.toLowerCase().includes(searchName) &&
+                  !user.last.toLowerCase().includes(searchName)) {
+                  return false
+                }
+              }
+              return true
+          }).map(user =>{
+            return(
+                <div style={{display:'flex', flexDirection:'column',justifyContent:'center',marginTop:'5px'}}>
+                    <div onClick={()=>this.startChat(userId,user)} key={user.id} style={{display:'flex', alignItems:'center',background:'orange',borderRadius:'10px',width:'98%',marginLeft:'1%'}}>
+                        <img src={user.pic} style={{height:'50px',width:'50px',borderRadius:'50%',marginLeft:'10px',marginRight:'10px'}} alt='pic'/>
+                        <h3 >{user.first} {user.last}</h3>
+                    </div>
+                </div>
+            )
+        })
         return(
             <div>
-                This is the Individual list.
+                {mappedChats}
             </div>
         )
     }
 } 
 
-export default Individuals;
+function mapStateToProps(reduxState){
+    return{
+        chats:reduxState.chats,
+        user:reduxState.user
+    }
+}
+
+export default connect(mapStateToProps,{selectedFriend,updateRoom})(Individuals);
