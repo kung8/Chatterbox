3b8e4b5b5fda5cd75e563302bdf1db8a6ad43053
Kevin Ung <ung.kevin78@gmail.com>
2019-06-29 18:26:36 -0600

    min height for message

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -47,7 +47,7 @@ class Chat extends Component {
                     position = "flex-end";
                     return (
                         <div key={message.id} style={{ width: "98%", display: "flex", justifyContent: `${position}`, marginRight: "5px" }}>
-                            <div style={{ background: `${color}`, display: "flex", marginTop: "5px", maxWidth: "60%", justifyContent: "flex-end", borderRadius: "10px", padding: "4px" }}>
+                            <div style={{ background: `${color}`, display: "flex", marginTop: "5px", maxWidth: "60%", justifyContent: "flex-end", borderRadius: "10px", padding: "4px" ,minHeight:40}}>
                                 <div style={{ display: "flex"}}>
                                     <p style={{ margin: 0, padding: 0, textAlign: "left", marginLeft: "2px" ,minHeight:'2rem'}}>{message.message}</p>
                                 </div>
@@ -204,7 +204,7 @@ const Chats = styled.div`
     display:flex;
     background:lightgrey;
     height:75%;
-    max-height:75%;
+    // max-height:75%;
     width:100%;
     flex-direction:column;
     overflow-y:scroll;
